757d776879e3f09d430ffafc5f9be20da7a451f1
GabriellaSwenson <unipuppydj@gmail.com>
2023-05-16 11:01:08 -0500

    HTML and Questions

---
+++ assets/js/questions.js
@@ -0,0 +1,38 @@
+let questions = [
+  {
+    title:
+      "What percent of websites as of 2022 use JavaScript on the client side?",
+    options: ["27%", "64%", "98%", "71%"],
+    answer: "98%",
+  },
+  {
+    title: "When was JavaScript first made",
+    options: ["2001", "1995", "1989", "1999"],
+    answer: "1995",
+  },
+  {
+    title: "How long did it take to initially create JavaScript",
+    options: ["10 days", "3 weeks", "20 days", "5 weeks"],
+    answer: "10 days",
+  },
+  {
+    title: "What was the original name for JavaScript",
+    options: ["Mocha", "CoffeeScript", "LiveScript", "Java-Script"],
+    answer: "Mocha",
+  },
+  {
+    title: "Which one of these is not a JavaScript framework?",
+    options: ["React", "Angular", "Django", "Vue"],
+    answer: "Django",
+  },
+  {
+    title: "Which one is not a JavaScript testing framework?",
+    options: ["Mocha", "Jasmine", "Chai", "React"],
+    answer: "React",
+  },
+  {
+    title: "Which company developed the first Javascript engine?",
+    options: ["Google", "Mozilla", "Microsoft", "Netscape"],
+    answer: "Netscape",
+  },
+];
